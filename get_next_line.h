2cbb650315c1f55dda5c8dc4d0fd8cff1c972935
CengizhanDisci <67480175+CengizhanDisci@users.noreply.github.com>
2022-04-25 21:57:33 +0300

    17.02.2022
    Get_next_line is a 42 Project and a useful function to read FDs.
    
    Return (1, 0 or -1) if there is respectively a new line, last line of the file, unable to read the file.
    Able to read multiple FDs.
    All memory used for the last line is fred.

---
+++ get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdisci <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/17 13:57:03 by cdisci            #+#    #+#             */
+/*   Updated: 2022/02/17 13:57:07 by cdisci           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*ft_strjoin(char *s1, const char *s2);
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *s);
+size_t	ft_strlen(const char *s);
+
+#endif
